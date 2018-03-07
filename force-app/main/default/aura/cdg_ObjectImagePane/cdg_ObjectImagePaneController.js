1f4a3648b1ed62cecc068b41edcaf4a4e3f9ebfb
Chris De Gour <cdegour@cdegour-ltm2.internal.salesforce.com>
2018-03-07 09:03:08 -0600

    rethinking the new file structure appropriately testing with stuff

--- force-app/main/default/aura/cdg_ObjectImagePane/cdg_ObjectImagePaneController.js
+++ force-app/main/default/aura/cdg_ObjectImagePane/cdg_ObjectImagePaneController.js
@@ -1,4 +1,16 @@
 ({
+    doInit : function(cmp, evt, helper){
+        //give helper the recordID
+        
+        var imageID = helper.getImageID(cmp.get("v.recordId"), 'recImage');
+        if(imageID) == null{
+            cmp.set("v.hasImage", false);
+        }else{
+            cmp.set("v.hasImage", true);
+            cmp.set("v.recordURL", )
+            // /sfc/servlet.shepherd/version/renditionDownload?rendition=ORIGINAL_Png&versionId=0683B000000Mlht&operationContext=CHATTER&contentId={!RECORDID}
+        }
+    }
     handleImageUpload : function(cmp, evt, helper) {
         //convert the pane to the display div not the input div
     }
--- force-app/main/default/aura/cdg_ObjectImagePane/cdg_ObjectImagePaneHelper.js
+++ force-app/main/default/aura/cdg_ObjectImagePane/cdg_ObjectImagePaneHelper.js
@@ -1,5 +1,28 @@
 ({
-    helperMethod : function() {
+    getImageID : function(recordID, filename) {
+        var action = cmp.get("c.getRecordImageID")
+        action.setParams({recordID:recordID, filename:filename});
 
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            if (state === "SUCCESS"){
+                console.log(response.getResponseValue());
+                return response.getResponseValue().Id;
+            }else if (state === "INCOMPLETE"){
+
+            }else if (state === "ERROR"){
+                var errors = response.getError();
+                if(errors){
+                    if(errors[0] && errors[0].message){
+                        console.log("Error Message: " + errors[0].message);
+                    }
+                    
+                }else{
+                    console.log("Unknown Error");
+                }
+            }
+        });
+
+        $A.enqueueAction(action);
     }
 })
