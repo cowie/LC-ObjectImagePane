9cc3c7094d3201b98245927e5d1960e309e05f35
Chris De Gour <cdegour@cdegour-ltm2.internal.salesforce.com>
2018-03-08 14:55:03 -0800

    initial build. upload, display, app builder variables and change.

--- force-app/main/default/aura/cdg_ObjectImagePane/cdg_ObjectImagePaneController.js
+++ force-app/main/default/aura/cdg_ObjectImagePane/cdg_ObjectImagePaneController.js
@@ -1,23 +1,20 @@
 ({
     doInit : function(cmp, evt, helper){
-        //give helper the recordID
-        /*
-        var imageID = helper.getImageID(cmp.get("v.recordId"), 'recImage');
-        if(imageID) == null{
-            cmp.set("v.hasImage", false);
-        }
-        else {
-            cmp.set("v.hasImage", true);
-            cmp.set("v.recordURL", )
-            // /sfc/servlet.shepherd/version/renditionDownload?rendition=ORIGINAL_Png&versionId=0683B000000Mlht&operationContext=CHATTER&contentId={!RECORDID}
-        }*/
+        var action = cmp.get("c.getURLPrefix");
+        action.setCallback(this, function(response){
+            cmp.set("v.baseURL", response.getReturnValue());
+            helper.getImage(cmp);
+        });
+        $A.enqueueAction(action);
     },
     handleImageUpload : function(cmp, evt, helper) {
         //convert the pane to the display div not the input div
         var uploadedFiles = evt.getParam("files");
-        console.log(uploadedFiles);
         var imageFile = uploadedFiles[0];
-        console.log(imageFile);
         helper.correctImageDeets(cmp, imageFile);
+    },
+    changeImageHandler : function(cmp, evt, helper){
+        //flip to upload pane
+        cmp.set("v.hasImage", false);
     }
 })
--- force-app/main/default/aura/cdg_ObjectImagePane/cdg_ObjectImagePaneHelper.js
+++ force-app/main/default/aura/cdg_ObjectImagePane/cdg_ObjectImagePaneHelper.js
@@ -1,26 +1,62 @@
 ({
     correctImageDeets : function(cmp, imageFile){
         var action = cmp.get("c.correctImageDeets");
-        action.setParams({recordID: imageFile.documentId, filename:cmp.get("v.filename")});
+        action.setParams({docID: imageFile.documentId, filename:cmp.get("v.filename")});
 
         action.setCallback(this, function(response){
-
+            console.log(response.getReturnValue());
+            var contentDoc = response.getReturnValue();
+            //setImageURL(cmp, contentDoc);
+            cmp.set("v.hasImage", true);
+            var baseURL = cmp.get("v.baseURL") + '/sfc/servlet.shepherd/version/renditionDownload?rendition=THUMB720BY480';
+            ///sfc/servlet.shepherd/version/renditionDownload?rendition=THUMB720BY480
+            baseURL = baseURL + '&versionId=' + contentDoc.LatestPublishedVersionId;
+            //&versionId=VersionID
+            baseURL = baseURL + '&contentId=' + contentDoc.Id;
+            //&contentId=contentID
+            cmp.set("v.imageURL", baseURL);
         });
 
         $A.enqueueAction(action);
     },
-    getImageID : function(recordID, filename) {
-        /*
-        var action = cmp.get("c.getRecordImageID")
+    setImageURL : function(cmp, doc){
+        cmp.set("v.hasImage", true);
+        var baseURL = cmp.get("v.baseURL") + '/sfc/servlet.shepherd/version/renditionDownload?rendition=THUMB720BY480';
+        ///sfc/servlet.shepherd/version/renditionDownload?rendition=THUMB720BY480
+        baseURL = baseURL + '&versionId=' + contentDoc.LatestPublishedVersionId;
+        //&versionId=VersionID
+        baseURL = baseURL + '&contentId=' + contentDoc.Id;
+        //&contentId=contentID
+        cmp.set("v.imageURL", baseURL);
+    },
+    getImage : function(cmp) {
+        var action = cmp.get("c.getRecordImage");
+        var recordID = cmp.get("v.recordId");
+        var filename = cmp.get("v.filename");
         action.setParams({recordID:recordID, filename:filename});
 
         action.setCallback(this, function(response){
             var state = response.getState();
             if (state === "SUCCESS"){
-                console.log(response.getResponseValue());
-                return response.getResponseValue().Id;
-            }else if (state === "INCOMPLETE"){
+                console.log(response.getReturnValue());
+                if(response.getReturnValue() != null){
 
+                
+                    var contentDoc = response.getReturnValue();
+                    //setImageURL(cmp, response.getReturnValue());
+                    cmp.set("v.hasImage", true);
+                    var baseURL = cmp.get("v.baseURL") + '/sfc/servlet.shepherd/version/renditionDownload?rendition=THUMB720BY480';
+                    ///sfc/servlet.shepherd/version/renditionDownload?rendition=THUMB720BY480
+                    baseURL = baseURL + '&versionId=' + contentDoc.LatestPublishedVersionId;
+                    //&versionId=VersionID
+                    baseURL = baseURL + '&contentId=' + contentDoc.Id;
+                    //&contentId=contentID
+                    cmp.set("v.imageURL", baseURL);
+                }else{
+                    //returned no results. Guess I do nothing? Probably. I'm tired.
+                }
+            }else if (state === "INCOMPLETE"){
+                console.log("incomplete?");
             }else if (state === "ERROR"){
                 var errors = response.getError();
                 if(errors){
@@ -35,6 +71,14 @@
         });
 
         $A.enqueueAction(action);
-        */
+
+    },
+
+    getBaseURL : function(cmp){
+        var action=cmp.get("c.getURLPrefix");
+        action.setCallback(this, function(response){
+            cmp.set("v.baseURL", response.getReturnValue());
+        });
+        $A.enqueueAction(action);
     }
 })
